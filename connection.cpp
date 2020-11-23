c3928be13a69a7185e46ad8001bd69b883d12526
Sami Ben Chaabane <29543098+SamiBenChaabane@users.noreply.github.com>
2020-11-23 16:35:05 +0100

    Update connection.cpp

--- connection.cpp
+++ connection.cpp
@@ -8,7 +8,7 @@ bool Connection::createconnect()
 {
     bool test=false;
     QSqlDatabase db =QSqlDatabase::addDatabase("QODBC");
-    db.setDatabaseName("Source_Projet2A");
+    db.setDatabaseName("projet");
     db.setUserName("Sami");
     db.setPassword("0000");
     if (db.open())
