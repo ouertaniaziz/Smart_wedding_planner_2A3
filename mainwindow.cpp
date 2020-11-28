c4b5b408a88cb84cecefe1d8614e7054a724b545
Sami Ben Chaabane <29543098+SamiBenChaabane@users.noreply.github.com>
2020-11-28 05:48:58 +0100

    Added new pages and add CRUD

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -60,3 +60,52 @@ void MainWindow::on_can_emp_clicked()
     ui->stackedWidget->setCurrentIndex(5);
 
 }
+void MainWindow::on_list_Musicians_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(2);
+}
+
+void MainWindow::on_commandLinkButton_4_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+}
+
+void MainWindow::on_sub_mus_clicked()
+{
+    int prix_mus = ui->PRMedit->text().toInt();
+    QString nom_mus = ui->FNedit->text();
+    QString prenom_mus = ui->LNedit->text();
+    int id_mus = ui->IDMedit->text().toInt();
+    QString genre_mus = ui->MGedit->text();
+    QString email_mus = ui->EMedit->text();
+    musiciens m(prix_mus, nom_mus, prenom_mus, id_mus, genre_mus, email_mus);
+    bool test = m.ajouter_mus();
+    if (test)
+    {
+        ui->PRMedit->clear();
+        ui->FNedit->clear();
+        ui->LNedit->clear();
+        ui->IDMedit->clear();
+        ui->MGedit->clear();
+        ui->EMedit->clear();
+        ui->tableView->setModel(mscs.afficher_mus());
+
+        QMessageBox::information(nullptr, QObject::tr("Success"), QObject::tr("Musician Successfully Added!\n"), QMessageBox::Ok);
+    }
+    else
+    {
+        QMessageBox::critical(nullptr, QObject::tr("Failed"), QObject::tr("Failed to add musician\n"
+                                                                          "Click cancel."),
+                              QMessageBox::Cancel);
+    }
+}
+
+void MainWindow::on_commandLinkButton_5_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+}
+
+void MainWindow::on_del_Musicians_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(5);
+}
