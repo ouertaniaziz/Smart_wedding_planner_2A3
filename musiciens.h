0d82b917f3f5016fe60e3c858a3a429e82e307db
Sami Ben Chaabane <29543098+SamiBenChaabane@users.noreply.github.com>
2020-12-17 13:31:13 +0100

    Update musiciens.h

--- musiciens.h
+++ musiciens.h
@@ -1,16 +1,16 @@
 #ifndef MUSICIENS_H
 #define MUSICIENS_H
-#include<QString>
-#include<QSqlQuery>
-#include<QSqlQueryModel>
+#include <QString>
+#include <QSqlQuery>
+#include <QSqlQueryModel>
 
 class musiciens
 {
-    QString nom_mus,prenom_mus,id_mus,genre_mus,email_mus;
-    float prix_mus;
+    QString nom_mus, prenom_mus, genre_mus, email_mus;
+    int prix_mus, id_mus;
 public:
     musiciens(){};
-    musiciens(float,QString,QString,QString,QString,QString);
+    musiciens(int, QString, QString, int, QString, QString);
     QString getNom_mus()
     {
         return nom_mus;
@@ -19,7 +19,7 @@ public:
     {
         return prenom_mus;
     }
-    QString getid_mus()
+    int getid_mus()
     {
         return id_mus;
     }
@@ -31,10 +31,45 @@ public:
     {
         return email_mus;
     }
-    float getprix_mus()
+    int getprix_mus()
     {
         return prix_mus;
     }
+
+    void setNom_mus(QString nm)
+    {
+        nom_mus = nm;
+    }
+    void setPrenom_mus(QString prm)
+    {
+        prenom_mus = prm;
+    }
+    void setid_mus(int idm)
+    {
+        this->id_mus = idm;
+    }
+    void setgenre_mus(QString gnm)
+    {
+        genre_mus = gnm;
+    }
+    void setemail_mus(QString emm)
+    {
+        email_mus = emm;
+    }
+    void setprix_mus(int prixm)
+    {
+        this->prix_mus = prixm;
+    }
+    bool ajouter_mus();
+    QSqlQueryModel *afficher_mus(QString nom_mus,int id_mus, int index);
+    bool supprimer_mus(int);
+    bool modifier_mus(int, int, QString, QString, QString, QString, int);
+    int affecter_mus(int);
+    QString affecter_mus2(int);
+    QString affecter_mus3(int);
+    QString affecter_mus4(int);
+    QString affecter_mus5(int);
+    int affecter_mus6(int);
 };
 
 #endif // MUSICIENS_H
