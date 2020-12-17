e471c966e61e8bf504e3cc79d97fee46b76f2335
Sami Ben Chaabane <29543098+SamiBenChaabane@users.noreply.github.com>
2020-12-17 13:35:23 +0100

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -1,6 +1,7 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-
+#include "musiciens.h"
+#include "employes.h"
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
@@ -16,44 +17,89 @@ public:
     ~MainWindow();
 
 private slots:
-    void on_pushButton_clicked();
+    void on_Mus_management_clicked();
 
-    void on_pushButton_2_clicked();
+    void on_backtosono_clicked();
 
-    void on_commandLinkButton_clicked();
+    void on_Consult_Musicians_clicked();
 
-    void on_commandLinkButton_2_clicked();
-
-    void on_add_Musicians_clicked();
-
-
-
-    void on_can_mus_clicked();
-
-    void on_add_Emp_clicked();
-
-    void on_can_emp_clicked();
-void on_list_Musicians_clicked();
-
-    void on_commandLinkButton_4_clicked();
+    void on_backtomusman_clicked();
 
     void on_sub_mus_clicked();
 
-    void on_commandLinkButton_5_clicked();
+    void on_Manage_Musicians_clicked();
 
-    void on_del_Musicians_clicked();
+    void on_backtosono_2_clicked();
+
+    void on_Add_mus_button_clicked();
+
+    void on_gobacktomanmus_clicked();
+
+    void on_can_mus_clicked();
+
+    void on_searchtomod_clicked();
+
+    void on_Modify_mus_button_clicked();
+
+    void on_sub_mus_2_clicked();
+
+    void on_backtomusman_2_clicked();
 
     void on_commandLinkButton_6_clicked();
 
     void on_delmusid_clicked();
 
-    void on_mod_Musicians_clicked();
+    void on_Delete_mus_button_clicked();
 
-    void on_commandLinkButton_7_clicked();
+    void on_search_clicked();
 
-    void on_sub_mus_2_clicked();
+    void on_reset_clicked();
+
+    void on_Sort_mus_activated(int index);
+
+    void on_Consult_Employees_clicked();
+
+    void on_Emp_management_clicked();
+
+    void on_searchname_2_clicked();
+
+    void on_comboBox_2_activated(int index);
+
+    void on_searchid_3_clicked();
+
+    void on_sub_emp_2_clicked();
+
+    void on_Add_emp_button_clicked();
+
+    void on_Manage_Employees_clicked();
+
+    void on_commandLinkButton_10_clicked();
+
+    void on_sub_mus_3_clicked();
+
+    void on_commandLinkButton_11_clicked();
+
+    void on_Modify_mus_button_2_clicked();
+
+    void on_commandLinkButton_12_clicked();
+
+    void on_commandLinkButton_8_clicked();
+
+    void on_commandLinkButton_9_clicked();
+
+    void on_commandLinkButton_13_clicked();
+
+    void on_delemp_2_clicked();
+
+    void on_commandLinkButton_14_clicked();
+
+    void on_Delete_emp_button_clicked();
+
+    void on_Gen_PDF_clicked();
+
 private:
     Ui::MainWindow *ui;
-        musiciens mscs;
+    musiciens mscs;
+    employes empy;
 };
 #endif // MAINWINDOW_H
