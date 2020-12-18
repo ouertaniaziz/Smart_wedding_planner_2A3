30c2115853986ae5ea7320eafa87ea1436ce2d53
Sami Ben Chaabane <29543098+SamiBenChaabane@users.noreply.github.com>
2020-12-18 13:43:38 +0100

    Added SONO ui elements

--- connection.cpp
+++ connection.cpp
@@ -10,8 +10,8 @@ Connection::Connection()
 bool Connection::createconnect()
 {bool test=false;
 QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
-db.setDatabaseName("Source_Projet2A");
-db.setUserName("system");//inserer nom de l'utilisateur
+db.setDatabaseName("projet");
+db.setUserName("sami");//inserer nom de l'utilisateur
 db.setPassword("0000");//inserer mot de passe de cet utilisateur
 
 if (db.open())
--- mainwindow.h
+++ mainwindow.h
@@ -15,7 +15,8 @@
 #include<QSqlQueryModel>
 #include"produit.h"
 #include <stdlib.h>
-
+#include "musiciens.h"
+#include "employes.h"
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -297,12 +298,93 @@ private slots:
     void on_aziz_clicked();
 
     void on_back_to_aziz_clicked();
+// SAMI SONO
+   void on_Mus_management_clicked();
 
+    void on_backtosono_clicked();
+
+    void on_Consult_Musicians_clicked();
+
+    void on_backtomusman_clicked();
+
+    void on_sub_mus_clicked();
+
+    void on_Manage_Musicians_clicked();
+
+    void on_backtosono_2_clicked();
+
+    void on_Add_mus_button_clicked();
+
+    void on_gobacktomanmus_clicked();
+
+    void on_can_mus_clicked();
+
+    void on_searchtomod_clicked();
+
+    void on_Modify_mus_button_clicked();
+
+    void on_sub_mus_2_clicked();
+
+    void on_backtomusman_2_clicked();
+
+    void on_commandLinkButton_6_clicked();
+
+    void on_delmusid_clicked();
+
+    void on_Delete_mus_button_clicked();
+
+    void on_search_clicked();
+
+    void on_reset_clicked();
+
+    void on_Sort_mus_activated(int index);
+
+    void on_Consult_Employees_clicked();
+
+    void on_Emp_management_clicked();
+
+    void on_searchname_2_clicked();
+
+    void on_comboBox_2_activated(int index);
+
+    void on_searchid_3_clicked();
+
+    void on_sub_emp_2_clicked();
+
+    void on_Add_emp_button_clicked();
+
+    void on_Manage_Employees_clicked();
+
+    void on_commandLinkButton_10_clicked();
+
+    void on_sub_mus_3_clicked();
+
+    void on_commandLinkButton_11_clicked();
+
+    void on_Modify_mus_button_2_clicked();
+
+    void on_commandLinkButton_12_clicked();
+
+    void on_commandLinkButton_8_clicked();
+
+    void on_commandLinkButton_9_clicked();
+
+    void on_commandLinkButton_13_clicked();
+
+    void on_delemp_2_clicked();
+
+    void on_commandLinkButton_14_clicked();
+
+    void on_Delete_emp_button_clicked();
+
+    void on_Gen_PDF_clicked();
 private:
     Ui::MainWindow *ui;
     Smtp* smtp ;
     QString msg;
     QString mail;
     QPropertyAnimation *animation;
+	musiciens mscs;
+    employes empy;
 };
 #endif // MAINWINDOW_H
